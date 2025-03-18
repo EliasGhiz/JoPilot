985e96518e638ce203190a90e5cf10f7e51ae29b
justinmmeans <justin.means@temple.edu>
2025-03-18 10:28:53 -0400

    Changed sample route to test react and flask connection.

--- backend-api/app/routes.py
+++ backend-api/app/routes.py
@@ -3,9 +3,11 @@ from flask import request, jsonify, render_template
 
 def initialize_app_endpoints(app):
     
-    #route for homepage
-    @app.route('/') #parameters reflect web url path. This will be the homepage
-    def home():
-        return render_template('home.html')
+    #test sending data to react
+    @app.route('/test') #parameters reflect web url path. This will be the homepage
+    def test_endpoint():
+        data={'Test Message from API'}
+        return jsonify(data)
+        
     
     
