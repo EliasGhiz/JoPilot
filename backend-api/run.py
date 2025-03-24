33396d2fa0c445c24e7b4e9c5548365a506717cb
Mutx <40292751+mutx@users.noreply.github.com>
2025-03-23 20:18:20 -0400

    Update to fix backend porting on Digital Ocean

--- backend-api/run.py
+++ backend-api/run.py
@@ -1,10 +1,12 @@
 # Runs instance of app
 
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    # Bind to all interfaces so external health checks can connect (for deploying on Digital Ocean)
-    # Note: For production, use a production-ready WSGI server like Gunicorn or similar.
-    app.run(host='0.0.0.0', debug=True)
+    # Use the PORT environment variable if defined, otherwise default to 5000.
+    port = int(os.environ.get("PORT", 5000))
+    # Bind to all interfaces to allow external access.
+    app.run(host='0.0.0.0', port=port, debug=True)
