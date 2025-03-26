7dd3f5e2f031ecdbaf26660cc2d382a6cfee1495
Stanley Huang <tuo60965@temple.edu>
2025-03-26 17:02:04 -0400

    Initialized database in init and run

--- backend-api/app/__init__.py
+++ backend-api/app/__init__.py
@@ -1,10 +1,17 @@
 #Used to initialize a flask instance and the endpoint routes
 from flask import Flask
 from flask_cors import CORS
+from database import db
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
     from . import routes
     routes.initialize_app_endpoints(app)
+
     return app
--- backend-api/run.py
+++ backend-api/run.py
@@ -1,6 +1,7 @@
 # Runs instance of app
 
 from app import create_app
+from app.database import db
 import os
 
 app = create_app()
@@ -8,5 +9,8 @@ app = create_app()
 if __name__ == '__main__':
     # Use the PORT environment variable if defined, otherwise default to 5000.
     port = int(os.environ.get("PORT", 5000))
+    # Creates database tables
+    with app.app_context():
+        db.create_all()
     # Bind to all interfaces to allow external access.
     app.run(host='0.0.0.0', port=port, debug=True)
