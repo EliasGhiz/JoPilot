1578c4faf11a4470ad001477583b9e4519c3f946
elias <tum98557@temple.edu>
2025-04-10 10:35:07 -0400

    test

--- backend-api/autofill/autofill.py
+++ backend-api/autofill/autofill.py
@@ -7,13 +7,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+#TEST
+
 
 # Set up Selenium Chrome driver
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 # Load the test form webpage
-driver.get("http://localhost:8000/test_form.html")
+driver.get("https://demoqa.com/text-box")
 time.sleep(3)  # Allow page to load
 
 # JSON file for storing autofill memory
